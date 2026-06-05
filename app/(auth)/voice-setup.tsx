80325f7f44db75603b532b0fd04fba7d56afd844
Olamilekan Aremu <aremu0235@gmail.com>
2026-06-05 17:54:48 +0100

    chore: locked react version

---
+++ app/(auth)/voice-setup.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function VoiceSetupScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Voice Setup</Text>
+    </View>
+  )
+}
